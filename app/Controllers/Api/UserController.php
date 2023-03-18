f77330f41302b2d72f9999b2913d9c3e242c4764
harrysudana <harrysudana@gmail.com>
2023-03-18 20:20:10 +0800

    using API Response trait

--- app/Controllers/Api/UserController.php
+++ app/Controllers/Api/UserController.php
@@ -3,13 +3,18 @@
 namespace App\Controllers\Api;
 
 use App\Controllers\BaseController;
+use CodeIgniter\API\ResponseTrait;
 
 class UserController extends BaseController
 {
+    use ResponseTrait;
+
     public function profile()
     {
         $user = auth()->user();
-        return $this->response
-            ->setJSON(['data' => $user]);
+        return $this->respond($user, 200);
+
+        //return $this->response
+        //    ->setJSON(['data' => $user]);
     }
 }
